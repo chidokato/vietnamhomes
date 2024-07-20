5aa15fa21f0b364fd794c9566772bbed95009aca
chidokato <tuan.pn92@gmail.com>
2024-07-20 12:32:19 +0700

    sdf

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -3,24 +3,23 @@
 use Intervention\Image\ImageManagerStatic as Image;
 use Illuminate\Support\Str;
 
-if (!function_exists('saveImage')) {
-    function saveImage($file, $path = 'data/product/', $width = 800, $height = 800) {
-        $originalFilename = $file->getClientOriginalName();
-        $filenameWithoutExtension = pathinfo($originalFilename, PATHINFO_FILENAME);
-        $extension = $file->getClientOriginalExtension();
-        $filename = $filenameWithoutExtension . '.' . $extension;
+function saveImage($file, $path = 'data/images/', $width = 800, $height = 800) {
+    $originalFilename = $file->getClientOriginalName();
+    $filenameWithoutExtension = Str::slug(pathinfo($originalFilename, PATHINFO_FILENAME), '-');
+    $extension = $file->getClientOriginalExtension();
+    $filename = $filenameWithoutExtension . '.' . $extension;
 
-        while (file_exists(public_path($path . $filename))) {
-            $filename = $filenameWithoutExtension . '_' . rand(0, 99) . '.' . $extension;
-        }
-
-        $img = Image::make($file)
-            ->resize($width, $height, function ($constraint) {
-                $constraint->aspectRatio();
-            })
-            ->save(public_path($path . $filename));
-
-        return $filename;
+    while (file_exists(public_path($path . $filename))) {
+        $filename = $filenameWithoutExtension . '_' . rand(0, 99) . '.' . $extension;
     }
+
+    $img = Image::make($file)
+        ->resize($width, $height, function ($constraint) {
+            $constraint->aspectRatio();
+        })
+        ->save(public_path($path . $filename));
+
+    return $filename;
 }
+
 

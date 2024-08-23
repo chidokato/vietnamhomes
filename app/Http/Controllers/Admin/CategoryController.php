1e74fd436e5c0671947a329362f8384fb90e5216
chidokato <tuan.pn92@gmail.com>
2024-08-23 10:00:05 +0700

    update 3

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Str;
 use App\Models\Category;
 use Image;
 use File;

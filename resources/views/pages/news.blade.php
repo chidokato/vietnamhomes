fa6171e32440633e5d024903ac48b59a8cc8bf29
chidokato <tuan.pn92@gmail.com>
2024-08-23 10:45:45 +0700

    update 3

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -48,65 +48,45 @@
 
 <section class="card-grid news-sec">
     <div class="container">
-        <h3 class="text-uppercase title-subpage">Thị trường bất động sản</h3>
+        <h3 class="text-uppercase title-subpage">{{ $data->name }}</h3>
         <div class="row">
             <div class="col-lg-9">
                 <div class="news-hightlight">
                     <div class="row">
-                        <div class="col-md-8">
-                            <a class="card-overlay outline-effect" href="news-detail.htm">
-                                <span class="card-overlay-img"><img src="assets/images/space-4.gif" alt="" class="w-100" style="background-image: url('http://localhost/www/vietnamhomes/public/data/images/camau.jpg');"></span>
-                                <div class="card-overlay-body">
-                                    <div class="card-overlay-body-wrap">
-                                        <div class="time-box">
-                                            <span>21/10</span>
-                                            <span><i class="icon-time me-1"></i>2022</span>
-                                        </div>
-                                        <p class="text-truncate-set text-truncate-set-2">Mật độ xay dựng là gì? Cách tính mật độ xây dựng chuẩn nhất 2022</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card-overlay-flex">
-                                <a class="card-overlay card-overlay-sm outline-effect" href="news-detail.htm">
-                                    <span class="card-overlay-img"><img src="assets/images/space-4.gif" alt="" class="w-100" style="background-image: url('http://localhost/www/vietnamhomes/public/data/images/camau.jpg');"></span>
+                        <div class="col-md-12">
+                            @foreach($post as $key => $val)
+                            @if($key < 3)
+                            <div class="iteam">
+                                <a class="card-overlay outline-effect {{ $key>0 ? 'card-overlay-sm':'' }} " href="news-detail.htm">
+                                    <span class="card-overlay-img"><img src="assets/images/space-4.gif" alt="" class="w-100" style="background-image: url('data/images/{{$val->img}}');"></span>
                                     <div class="card-overlay-body">
                                         <div class="card-overlay-body-wrap">
                                             <div class="time-box">
                                                 <span>21/10</span>
                                                 <span><i class="icon-time me-1"></i>2022</span>
                                             </div>
-                                            <p class="text-truncate-set text-truncate-set-2">Mật độ xay dựng là gì? Cách tính mật độ xây dựng chuẩn nhất 2022</p>
-                                        </div>
-                                    </div>
-                                </a>
-                                <a class="card-overlay card-overlay-sm outline-effect" href="news-detail.htm">
-                                    <span class="card-overlay-img"><img src="assets/images/space-4.gif" alt="" class="w-100" style="background-image: url('http://localhost/www/vietnamhomes/public/data/images/camau.jpg');"></span>
-                                    <div class="card-overlay-body">
-                                        <div class="card-overlay-body-wrap">
-                                            <div class="time-box">
-                                                <span>21/10</span>
-                                                <span><i class="icon-time me-1"></i>2022</span>
-                                            </div>
-                                            <p class="text-truncate-set text-truncate-set-2">Mật độ xay dựng là gì? Cách tính mật độ xây dựng chuẩn nhất 2022</p>
+                                            <p class="text-truncate-set text-truncate-set-2">{{$val->name}}</p>
                                         </div>
                                     </div>
                                 </a>
                             </div>
+                            @endif
+                            @endforeach
                         </div>
                     </div>
                 </div>
                 
                 <div class="row row-cols-1 g-4 horizontal-view" id="show-setting">
+                    @foreach($post as $key => $val)
+                    @if($key > 2)
                     <div class="col">
                         <div class="card card-s card-s4">
                             <a href="news-detail.htm">
-                                <span><img src="assets/images/space-3.gif" class="card-img-top" style="background-image: url('http://localhost/www/vietnamhomes/public/data/images/camau.jpg');" alt="..."></span>
+                                <span><img src="assets/images/space-3.gif" class="card-img-top" style="background-image: url('data/images/{{$val->img}}');" alt="..."></span>
                             </a>
                             <div class="card-body">
                                 <div class="card-body-wrap">
-                                    <h5 class="card-title"><a href="news-detail.htm">Vì sao giá bất động sản ven sông lại có biên độ tăng giá tốt nhất TP.HCM hiện nay?</a></h5>
+                                    <h5 class="card-title"><a href="news-detail.htm">{{$val->name}}</a></h5>
                                     <div class="card-info">
                                         <span><i class="icon-time me-2"></i>2 ngày trước</span>
                                         <span><i class="icon-user me-2"></i>Minh Hoàng</span>
@@ -116,6 +96,8 @@
                             </div>
                         </div>
                     </div>
+                    @endif
+                    @endforeach
 
                    
                     
@@ -127,7 +109,7 @@
             <div class="col-lg-3 d-none d-lg-block">
 
                 <div class="widget widget-list mb-3">
-                    <h4><span>Tin tức</span></h4>
+                    <!-- <h4><span>Tin tức</span></h4> -->
                     <ul>
                         <li><a href="tin-thi-truong"><i class="icon-next me-2"></i>Tin thị trường</a></li>
                         <li><a href="tin-noi-bo"><i class="icon-next me-2"></i>Tin nội bộ</a></li>
@@ -137,27 +119,30 @@
                 
                 <div class="widget widget-list widget-news mb-3">
                     <h4><span>Tin xem nhiều</span></h4>
+                    @foreach($post as $key => $val)
+                    @if($key == 0)
                     <a href="#" class="news-item-captain">
                         <div class="news-item-captain-img">
                             <div class="news-item-captain-img-wrap">
-                                <img src="assets/images/space-3.gif" style="background-image: url('http://localhost/www/vietnamhomes/public/data/images/camau.jpg');" alt="" class="w-100">
+                                <img src="assets/images/space-3.gif" style="background-image: url('data/images/{{$val->img}}');" alt="" class="w-100">
                                 <span class="date"><i class="icon-time me-1"></i>2 ngày trước</span>
-                                <span class="view-more">Chi tiết</span>
                             </div>
                         </div>
                         <div class="news-item-captain-body">
-                            <h5>Review phường Hoàng Liệt bản đồ, vị trí, các tiện ịch xung quanh</h5>
+                            <h5>{{$val->name}}</h5>
                             <p class="mb-0  text-truncate-set text-truncate-set-2">Chính chủ cần chuyển nhượng gấp căn 2 ngủ diện tích thông thủy 78m2 full đồ, khách mua chỉ cần dọn quần áo đến có thể ở ngay</p>
                         </div>
                     </a>
+                    @else
                     <a href="#" class="news-item">
-                        <span><img src="assets/images/space-3.gif" style="background-image: url('http://localhost/www/vietnamhomes/public/data/images/camau.jpg');" alt="" class="w-100"></span>
+                        <span><img src="assets/images/space-3.gif" style="background-image: url('data/images/{{$val->img}}');" alt="" class="w-100"></span>
                         <div class="news-item-body">
                             <span class="date"><i class="icon-time me-1"></i>2 ngày trước</span>
-                            <p class="mb-0 text-truncate-set text-truncate-set-2">Chính chủ cần chuyển nhượng gấp căn hộ diện tích 80m2</p>
+                            <p class="mb-0 text-truncate-set text-truncate-set-2">{{$val->name}}</p>
                         </div>
                     </a>
-                    
+                    @endif
+                    @endforeach
                 </div>
 
             </div>
